6195bc90d2f192708b3b65fe88083e7ddaae630c
AndreKaled <andrekaled9@gmail.com>
2025-10-18 17:46:53 -0400

    pontos e coordenadas funcionais, falta a otimização

---
+++ java-express/src/main/java/com/example/javaexpress/model/util/RotaOtimizada.java
@@ -0,0 +1,13 @@
+package com.example.javaexpress.model.util;
+
+import com.example.javaexpress.model.model.Coordenadas;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+public class RotaOtimizada {
+
+    public List<Coordenadas> vizinhoMaisProximo(List<Coordenadas> pontos, double[][] matrizDistancias){
+        return pontos;
+    }
+}
